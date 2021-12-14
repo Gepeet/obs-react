2969b594f60009ce6b9b3192ab4e6a5ab253b832
vendefault <letskeddotcom@gmail.com>
2021-12-14 19:22:03 +0800

    removed console logs

--- src/PreviewScene.jsx
+++ src/PreviewScene.jsx
@@ -3,8 +3,6 @@ import './PreviewScene.css'
 
 const PreviewScene = (currentScene,media) => {
 
-    console.log(currentScene.currentScene[0])
-    console.log(media.media)
     return (
         <div className="previewPage">
             <img src={currentScene.currentScene[0]} alt={currentScene.currentScene[0]} className="previewContainer"/>
